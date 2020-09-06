ab111a2c17649b8db928fb2a0c4704d105dcf66f
Izumi Kawashima <schedul.xor@gmail.com>
2020-09-06 15:30:28 +0900

    Supported pre-defined keys

--- run.sh
+++ run.sh
@@ -7,13 +7,13 @@ TILESIZE=256
 # Y=1
 # Z=1
 
-Z=15
-X=29209
-Y=12561
+Z=16
+X=58211
+Y=25806
 
 mkdir -p tmp
 mkdir -p tmp/mvt
-wget --no-check-certificate -O tmp/mvt/${Z}_${X}_${Y}.mvt https://tile.nextzen.org/tilezen/vector/v1/256/all/${Z}/${X}/${Y}.mvt?api_key=${NEXTZEN_API_KEY}
+wget -nc --no-check-certificate -O tmp/mvt/${Z}_${X}_${Y}.mvt https://tile.nextzen.org/tilezen/vector/v1/256/all/${Z}/${X}/${Y}.mvt?api_key=${NEXTZEN_API_KEY}
 
 # mkdir -p tmp/oscimv4
 # mkdir -p tmp/oscimv4_geojson
