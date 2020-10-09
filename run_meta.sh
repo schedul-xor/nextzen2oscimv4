0b3322e8b8a94babb166b25d20e13309f3ecc86e
Izumi Kawashima <schedul.xor@gmail.com>
2020-10-09 16:57:32 +0900

    Added meta cases

--- run_meta.sh
+++ run_meta.sh
@@ -1,18 +1,24 @@
 #!/usr/bin/env sh
 
-# sh run.sh 10 909 403
-# sh run.sh 10 909 404
-# sh run.sh 10 910 404
+sh run.sh 10 909 403
+sh run.sh 10 909 404
+sh run.sh 10 910 404
 
-# sh run.sh 14 14552 6454
-# sh run.sh 14 14552 6455
-# sh run.sh 14 14553 6455
+sh run.sh 12 3638 1614
 
-# sh run.sh 15 29096 12914
-# sh run.sh 15 29097 12914
-# sh run.sh 15 29097 12915
+sh run.sh 14 14552 6454
+sh run.sh 14 14552 6455
+sh run.sh 14 14553 6455
+
+sh run.sh 15 29096 12914
+sh run.sh 15 29097 12914
+sh run.sh 15 29097 12915
 
 sh run.sh 15 29092 12900 # Ogikubo
+sh run.sh 17 116369 51602 # Ogikubo-overzoomed
+sh run.sh 17 116370 51602 # Ogikubo-overzoomed
 sh run.sh 17 116371 51602 # Ogikubo-overzoomed
 sh run.sh 16 58186 25804
 sh run.sh 16 58201 25805 # Shinjuku-gyoen
+
+sh run.sh 15 29105 12905
