5cc09aa0557f79286b3ab4512c1896e409070e5f
Izumi Kawashima <schedul.xor@gmail.com>
2020-10-13 10:06:53 +0900

    Forcely create all tmp files

--- app.py
+++ app.py
@@ -18,9 +18,10 @@ TMP_PATH = './tmp'
 MVT_CACHE_DIR = os.path.join(TMP_PATH,'mvt')
 GEOJSON_CACHE_DIR = os.path.join(TMP_PATH,'geojson')
 
-if not os.path.exists(TMP_PATH): os.mkdir(TMP_PATH)
-if not os.path.exists(MVT_CACHE_DIR): os.mkdir(MVT_CACHE_DIR)
-if not os.path.exists(GEOJSON_CACHE_DIR): os.mkdir(GEOJSON_CACHE_DIR)
+if not os.path.exists(TMP_PATH):
+    os.mkdir(TMP_PATH)
+    os.mkdir(MVT_CACHE_DIR)
+    os.mkdir(GEOJSON_CACHE_DIR)
 
 app = Flask(__name__)
 
