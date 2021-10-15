0f7d714115ff5da04415f7e1363fcbf58d8f7430
Izumi Kawashima <schedul.xor@gmail.com>
2021-10-15 21:22:03 +0900

    Remove nextzen

--- run.sh
+++ run.sh
@@ -2,14 +2,8 @@
 
 set -e
 
-#TARGETTYPE=nextzen
 TARGETTYPE=mvt
-#NEXTZEN_API_KEY=_lQbucvFRf6L7cPYIG1Fdg
 TILESIZE=256
-
-# X=1
-# Y=1
-# Z=1
 
 Z=$1
 X=$2
@@ -18,18 +12,12 @@ Y=$3
 MVT_BUFFER_PIXELS=5
 OSCIMV4_BUFFER_PIXELS=5
 
-#MVT_URL=https://tile.nextzen.org/tilezen/vector/v1/256/all/{0}/{1}/{2}.mvt?api_key=_lQbucvFRf6L7cPYIG1Fdg
 MVT_URL=http://tile0.ogiqvo.com:22380/maps/osm/${Z}/${X}/${Y}.pbf
 #MVT_URL=http://localhost:22380/maps/osm/${Z}/${X}/${Y}.pbf
 
 mkdir -p tmp
 mkdir -p tmp/${TARGETTYPE}
 wget -nc --no-check-certificate -O tmp/${TARGETTYPE}/${Z}_${X}_${Y}_${MVT_BUFFER_PIXELS}.mvt ${MVT_URL}
-
-# mkdir -p tmp/oscimv4
-# mkdir -p tmp/oscimv4_geojson
-# python convert_${TARGETTYPE}2oscimv4.py tmp/${TARGETTYPE}/${Z}_${X}_${Y}.mvt ${Z} ${X} ${Y} ${TILESIZE} tmp/oscimv4/${Z}_${X}_${Y}.vtm
-# python dump_oscim.py tmp/oscimv4/${Z}_${X}_${Y}.vtm ${Z} ${X} ${Y} tmp/oscimv4_geojson/${Z}_${X}_${Y}.json
 
 # Use tippecanoe to decode mvt to GeoJSON
 # https://github.com/mapbox/tippecanoe
