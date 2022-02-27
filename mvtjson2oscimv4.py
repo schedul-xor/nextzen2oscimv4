49f2a51f02e896b913d68d61a4dd3853f37254ea
Izumi Kawashima <schedul.xor@gmail.com>
2022-02-27 19:18:38 +0900

    Fixed again

--- mvtjson2oscimv4.py
+++ mvtjson2oscimv4.py
@@ -551,7 +551,8 @@ def convert(tile_z,tile_x,tile_y,buffer_pixels,fr):
                 for explicit_kind in frozenset(['waterway']):
                     if explicit_kind in kv: fixed_kv[explicit_kind] = kv[explicit_kind]
                 if 'waterway' in kv:
-                    fixed_kv['waterway'] = kv['waterway']
+                    if kv['waterway'] == 'stream':
+                        fixed_kv['waterway'] = 'river'
 
                 if 'type' in kv:
                     type_ = kv['type']
